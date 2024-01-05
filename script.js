aefcb19178e86f4bca3aa2f81b4f3da37a73513a
Onur Sönmez <onursnmz@outlook.com>
2024-01-05 14:01:07 +0300

    up_script_json

--- script.js
+++ script.js
@@ -13,10 +13,11 @@ search.addEventListener('click' , () => {
 
         return;
     
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&
-    appid=${APIkey}`).then(response => response.json()).then(json =>{
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIkey}`)
+    .then(response => response.json())
+    .then(json =>{
 
-        const image = document.querySelector('.weather-box .img');
+        const image = document.querySelector('.weather-box img');
         const temperature = document.querySelector('.weather-box .temperature');
         const description = document.querySelector('.weather-box .description');
         const humidity = document.querySelector('.weather-details .humidity');
@@ -50,6 +51,11 @@ search.addEventListener('click' , () => {
             default:
                 image.src = 'img/cloud.png';
         }
+
+        temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+        description.innerHTML = `${json.weather[0].description}`;
+        humidity.innerHTML = `${json.main.humidity} %`;
+        wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
         
     });
 });
